41a93d49f5a03cf998ec19a279c972e390d23b53
PiyJ <Piyush.Jadhav@dyson.com>
2021-08-11 19:13:59 +0100

    Static member and function.

--- task/static.cpp
+++ task/static.cpp
@@ -1,36 +1,43 @@
 // CPP program to illustrate
 // concept of Virtual Functions
 
+// static member function
+// static members exist as members of the class rather than as an instance
+// *this* keyword is not available within the static member function
+// The functions can be used without even creating the objects
+// The member functions cannot be virtual, const or volatile.
+// The address cannot be stored in pointer to member function address.
+
+// static data member
+// The static data member - has only one instance shared by all the objects created
+// Need to be initialed ( usually outside the class which implements it.)
+// The can be accessed using class::my_static_p or T.my_static_param or T->my_static_p, based on T evaluating my class..
+
 #include <iostream>
 using namespace std;
 
-void * p;
-
 class base {
 public:
-    base( )
-    {
-        cout << "Init the base class" << endl;
-    }
+	base( )
+	{
+		cout << "Constructor of the base class" << endl;
+	}
 
-	virtual void print()    
+	static void print()
 	{
 		cout << "print base class" << endl;
 	}
 
-    virtual void a ( int inputp) = 0;
+	private:
+	static int static_var;
 
-	void show()
-	{
-		cout << "show base class" << endl;
-	}
 };
+
+int base::static_var = 0;
 
 int main()
 {
-	base* bptr;  
-    
-	// Non-virtual function, binded at compile time
+	base* base_ptr;
 }
 
 
