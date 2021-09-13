f2648785f7bcf97e6e65808c5dbdb4052ca7cdf0
pio <piyushjadhav13@gmail.com>
2021-09-13 22:00:03 +0100

    bit add.

--- designs/factory-abstract/abstract_factory.cpp
+++ designs/factory-abstract/abstract_factory.cpp
@@ -7,22 +7,58 @@ using namespace std;
 
 class factory_af
 {
+    public:
+    factory_af()
+    {
+        cout << "Constructor of factory_af" << endl;
+    }
 
+    ~factory_af()
+    {
+        cout << "Destructor of factory_af" << endl;
+    }
+
+    chair C;
+    sofa S;
 };
 
 class factory_victorian : public factory_af
 {
+    factory_victorian()
+    {
+        cout << "Constructor of factory_vict" << endl;
+    }
 
+    ~factory_victorian()
+    {
+        cout << "Destructor of factory_vict" << endl;
+    }
 };
 
-class factory_modern : public factory_af
+class factory_mod : public factory_af
 {
+    factory_mod()
+    {
+        cout << "Constructor of factory_mod" << endl;
+    }
 
+    ~factory_mod()
+    {
+        cout << "Destructor of factory_mod" << endl;
+    }
 };
 
 class chair
 {
+    chair()
+    {
 
+    }
+
+    ~chair()
+    {
+
+    }
 };
 
 class chair_victorian : public chair
@@ -53,6 +89,8 @@ class sofa__modern: public sofa
 int main( void )
 {
     // AF 
+
+
 
     return 0;
 }
